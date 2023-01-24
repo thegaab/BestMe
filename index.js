e9f97dbb0876a0528eb2a978a9c02236db819397
Tiago Gabriel <tiagogabrielcontato@hotmail.com>
2023-01-24 19:14:06 -0300

    first commit BestME

---
+++ index.js
@@ -0,0 +1,40 @@
+const questions = [
+  "O que aprendi hoje?",
+  "O que me deixou aborrecido? E o que eu poderia fazer para melhorar?",
+  "O que me deixou feliz hoje?",
+  "Quantas pessoas ajudei hoje?",
+]
+
+const ask = (index = 0) =>{
+  process.stdout.write("\n\n" + questions[index] + "> ")
+}
+ask()
+
+const answers = []
+process.stdin.on("data", data =>{
+  answers.push(data.toString().trim())
+  if(answers.length < questions.length){
+    ask(answers.length)
+  } else {
+    console.log(answers)
+    process.exit()
+  }
+})
+process.on('exit', () =>{
+  console.log(`
+  Bacana Tigas!
+
+  Hoje você aprendeu: ${answers[0]}
+  
+
+  O que te aborreceu e você poderia melhorar foi: ${answers[1]}
+  
+
+  O que te deixou feliz hoje: ${answers[2]}
+  
+
+  Você ajudou ${answers[3]} pessoas hoje!
+
+  Espero te ver aqui amanhã!
+  `)
+})
